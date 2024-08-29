f6d6dc1e77036005318f85bb2d13b7ec10bb1b63
Angela Basnet <angellabasnet@gmail.com>
2024-08-29 17:37:29 +0545

    resized chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -27,7 +27,7 @@ const chartConfig = {
 
 export function Chart() {
   return (
-    <div className="bg-[#131731] p-8 w-[50%] rounded-3xl">
+    <div className="bg-[#131731] p-8 md:w-[50%] sm:w-[70%] w-full rounded-3xl">
       <div className="flex justify-between">
         <h2 className="text-white font-medium text-lg">TimeSheet</h2>
         {/* <div className="flex gap-2 py-2 px-4 border-2 border-gray-400 rounded-3xl">
@@ -37,7 +37,7 @@ export function Chart() {
       </div>
       <ChartContainer
         config={chartConfig}
-        className="max-h-[300px] max-w-[500px]"
+        className="max-h-[500px] max-w-[500px]"
       >
         <BarChart
           accessibilityLayer
